201c94e76ed4c10f21bf441505a91436fd1d8427
minwe <minwe@yunshipei.com>
2015-04-15 14:20:27 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# qn-store
-Qiniu store for Ghost blog.
+# Ghost Qiniu Storage
+
+
+This custom storage module allows you to store media file at [Qiniu](http://www.qiniu.com/) instead of storing at local machine, especially helpful for ghost blog hosted at heroku (no local storage). Will work with latest version `0.6.0` of Ghost!
+
+## Installation
+
+In order to replace the storage module, the basic requirements are:
+
+- Create a new folder inside `/content` called `/storage`
+- Clone this repo to `/storage`
+
+  ```
+  cd [path/to/ghost]/content/storage
+  git clone https://github.com/Minwe/qn-store.git
+  ```
+- Install dependencies
+
+  ```
+  cd qn-store
+  npm install
+  ```
+
+  You can add `qn` to dependencies in Ghost's `package.json`.
+
+## Configuration
+
+In your `config.js` file, you'll need to add a new `storage` block to whichever environment you want to change:
+
+```js
+storage: {
+  active: 'qn-store',
+  'qn-store': {
+    accessKey: 'your access key',
+    secretKey: 'your secret key',
+    bucket: 'your bucket name',
+    domain: 'http://xx.xx.xx.glb.clouddn.com',
+    // timeout: 3600000, // default rpc timeout: one hour, optional
+    // if your app outside of China, please set `uploadURL` to `http://up.qiniug.com/`
+    // uploadURL: 'http://up.qiniu.com/'
+    filePath: 'YYYY/MM/' // default `YYYY/MM/`, will be formated by moment.js, using `[]` to escape
+  }
+}
+```
+
+## License
+
+Read [LICENSE](LICENSE)
---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// # Qiniu storage module for Ghost blog http://ghost.org/
+
+var path = require('path');
+var fs  = require('fs-extra');
+var Promise = require('bluebird');
+var qn = require('qn');
+var moment = require('moment');
+var utils = require('../../../core/server/utils');
+
+function QiniuStore(config) {
+  this.options = config || {};
+}
+
+// ### Save
+// Saves the image to Qiniu
+// - image is the express image object
+// - returns a promise which ultimately returns the full url to the uploaded image
+QiniuStore.prototype.save = function(image) {
+  var _this = this;
+
+  return new Promise(function (resolve, reject) {
+    var client = qn.create(_this.options);
+    var key = _this.getFileKey(image);
+
+    client.upload(fs.createReadStream(image.path), {
+      key: key
+    }, function(err, result) {
+      if (err) {
+        reject('[' + err.code + '] ' + err.name);
+      } else {
+        resolve(result.url)
+      }
+    });
+  });
+};
+
+// middleware for serving the files
+QiniuStore.prototype.serve = function() {
+  // a no-op, these are absolute URLs
+  return function (req, res, next) {
+    next();
+  };
+};
+
+QiniuStore.prototype.getFileKey = function(image) {
+  var prefix = moment().format(this.options.filePath || 'YYYY/MM/').
+      replace(/^\//, '');
+  var ext = path.extname(image.name);
+  var name = utils.safeString(path.basename(image.name, ext));
+
+  return prefix + name + '-' + Date.now() + ext.toLowerCase();
+};
+
+/*
+QiniuStore.prototype.exists = function(filename) {
+  return new Promise(function (resolve, reject) {
+    // send key to get image info
+    client.stat(filename, function(err, info) {
+      if (info) {
+        resolve(true);
+      } else if  (err && err.code === 612) { // File not exists
+        resolve(false);
+      } else {
+        reject('Can\'t get file info.');
+      }
+    });
+  });
+};
+*/
+
+module.exports = QiniuStore;
